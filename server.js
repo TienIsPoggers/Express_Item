fdc06e362b5f87fe601fc087e50d4a16f1958e8f
TienIsPoggers <hangquoctien2008@gmail.com>
2025-06-04 01:22:47 +0700

    06 commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 8383;
-
+app.use(express.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // Cho phép tất cả các miền
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -10,7 +10,10 @@ app.use((req, res, next) => {
 app.get('/',(req,res) => {
     res.send("<h1>Hello World</h1>")
 })
-app.get('/test',(req,res) => {
-    res.json({message:"Hello World this is file"});
+app.post('/sign',(req,res) => {
+    const {username,password} = req.body;
+    if(username.length <= 6) res.json({message:"username it too short"});
+    if(password.length <= 6) res.json({message:"password it too short"});
+    res.json({message:`Success sign in`,username:username,password:password});
 })
 app.listen(PORT,() => {console.log("Server started with PORT:" + PORT)});
