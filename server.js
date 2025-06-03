9e1c66dd87b2aca461ff86be3b8010c4c068f144
TienIsPoggers <hangquoctien2008@gmail.com>
2025-06-04 00:57:48 +0700

    04 commit

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const PORT = process.env.PORT || 8383;
 app.get('/',(req,res) => {
     res.send("<h1>Hello World</h1>")
 })
+app.get('/test',(req,res) => {
+    res.json({message:"Hello World this is file"});
+})
 app.listen(PORT,() => {console.log("Server started with PORT:" + PORT)});
