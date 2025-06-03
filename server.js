448ce63e9f5b660d7250832fa28d68f92a326f0d
TienIsPoggers <hangquoctien2008@gmail.com>
2025-06-04 00:51:49 +0700

    second commit

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import path, {dirname} from 'path'
-import { fileURLToPath } from 'url';
-
 const app = express();
 const PORT = process.env.PORT || 8383;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'));
+    res.send("<h1>Hello World</h1>")
 })
 app.listen(PORT,() => {console.log("Server started with PORT:" + PORT)});
